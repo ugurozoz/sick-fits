e78cd7fa3ca3de3d52b137d451119a89d9a6d1de
ugur ozoz <ugurozoz@yahoo.com>
2021-05-01 00:45:49 +0300

    creating header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,12 +1,34 @@
-rom 'react-native'
+import Link from 'next/link';
+import styled from 'styled-components';
+import Nav from './Nav';
+
+const Logo = styled.h1`
+  background: red;
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
 
 export default function Header() {
   return (
     <header>
-      <Text></Text>
+      <div className="bar">
+        <Logo>
+          <Link href="/">Sick fits</Link>
+        </Logo>
+      </div>
+      <div className="sub-bar">
+        <p>Search</p>
+      </div>
+      <Nav />
     </header>
-  )
+  );
 }
-
-
-
